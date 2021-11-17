9f0584d32b20dc059cccb6455c2c332a21b2d5cb
root <root@AmRen-SriLi.localdomain>
2021-11-17 22:24:41 +0700

    utsno4

---
+++ utssemester3/soal4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float tahunlahir = 2000;
+    float tahunsekarang = 2021;
+    int usia = tahunsekarang - tahunlahir;
+    
+    cout<<"Berarti Usia kalian adalah = "<<usia<<" Tahun"<<endl;
+
+    return 0;
+}
